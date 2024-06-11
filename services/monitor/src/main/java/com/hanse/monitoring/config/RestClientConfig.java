52daf98d6565da76b372e6aea56d1003a41f1512
Eduardo Barros <eduardo.barros@retarus.com>
2024-06-12 00:12:23 +0100

    more code

--- services/monitor/src/main/java/com/hanse/monitoring/config/RestClientConfig.java
+++ services/monitor/src/main/java/com/hanse/monitoring/config/RestClientConfig.java
@@ -2,6 +2,7 @@ package com.hanse.monitoring.config;
 
 import com.hanse.monitoring.analytics.client.AnalyticsHttpClient;
 import com.hanse.monitoring.configuration.client.ConfigurationHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -11,16 +12,21 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class RestClientConfig {
 
+    @Value("${client.url.configuration-server}")
+    private String configurationServiceUrl;
+    @Value("${client.url.analytics-server}")
+    private String analyticsServiceUrl;
+
     @Bean
     ConfigurationHttpClient configurationHttpClient() {
-        RestClient restClient = RestClient.create("http://localhost:8070/api/v1");
+        RestClient restClient = RestClient.create(configurationServiceUrl);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
         return factory.createClient(ConfigurationHttpClient.class);
     }
 
     @Bean
     AnalyticsHttpClient analyticsHttpClient() {
-        RestClient restClient = RestClient.create("http://localhost:8071/api/v1");
+        RestClient restClient = RestClient.create(analyticsServiceUrl);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
         return factory.createClient(AnalyticsHttpClient.class);
     }
